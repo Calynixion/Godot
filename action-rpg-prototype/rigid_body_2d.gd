4cbfb26719b333850cb325a47bed00f0f78cc9c5
Fwablo <fabriziomm2009@gmail.com>
2025-06-25 17:58:53 -0400

    added enemy(poorly)

--- action-rpg-prototype/rigid_body_2d.gd
+++ action-rpg-prototype/rigid_body_2d.gd
@@ -2,11 +2,12 @@ extends RigidBody2D
 
 #@onready var target = get_tree().get_node("player")
 @onready var target = get_tree().get_first_node_in_group("Targets") 
+const speed = 10
 
 func _physics_process(delta):
 	if target==null:
 		pass
 	else:
-		pass
+		add_constant_force((target.position - position).normalized() * delta * speed)
 		
 	
