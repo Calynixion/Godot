cefc49ab8a8663288278aabf05d17aad3b6e15a4
Calynixion <160431995+Calynixion@users.noreply.github.com>
2025-06-25 17:23:15 -0400

    Basic Player Animations

--- action-rpg-prototype/player.gd
+++ action-rpg-prototype/player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
-var speed = 500 
+@onready var sprite=get_node("sprite")
+
+var speed = 500
+
 func _physics_process(_delta):
 	# setup direction of movement
 	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction==Vector2(0,0):
+		sprite.animation="idle"
+	else:
+		sprite.play("walk")
 	#normalize the directional movement
 	direction = direction.normalized()
 	# setup the actual movement
