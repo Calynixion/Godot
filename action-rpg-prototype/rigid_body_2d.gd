690f3c24fec784d911e8075bb6a612d2321801f8
Calynixion <160431995+Calynixion@users.noreply.github.com>
2025-06-25 17:46:15 -0400

    Fixed Enemy Targets

--- action-rpg-prototype/rigid_body_2d.gd
+++ action-rpg-prototype/rigid_body_2d.gd
@@ -1,5 +1,12 @@
 extends RigidBody2D
 
-@onready var player = get_tree().get_node("player")
+#@onready var target = get_tree().get_node("player")
+@onready var target = get_tree().get_first_node_in_group("Targets") 
 
 func _physics_process(delta):
+	if target==null:
+		pass
+	else:
+		pass
+		
+	
