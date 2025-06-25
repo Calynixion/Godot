031d6c3c24c5d9bafe8f5d31a6feb2e018068d78
Calynixion <160431995+Calynixion@users.noreply.github.com>
2025-06-25 17:14:16 -0400

    Added Basic Player Scene

---
+++ action-rpg-prototype/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+var speed = 500 
+func _physics_process(_delta):
+	# setup direction of movement
+	var direction = Input.get_vector("left", "right", "up", "down")
+	#normalize the directional movement
+	direction = direction.normalized()
+	# setup the actual movement
+	velocity = (direction * speed)
+	move_and_slide()
