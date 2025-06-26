daf189f9326f11d05965c0ed461d1a5ca2a77dd6
Fwablo <fabriziomm2009@gmail.com>
2025-06-26 16:25:00 -0400

    made basic enemy(finally)

--- chrono-cassette/enemy.gd
+++ chrono-cassette/enemy.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var ray_cast2 = $RayCast2D2
 @onready var ray_cast3 = $RayCast2D3
 @onready var ray_cast4 = $RayCast2D4
+@onready var ray_cast5 = $RayCast2D5
 
 #follow path
 #if distraction then save point on path and go to distraction
@@ -11,15 +12,19 @@ extends CharacterBody2D
 
 func _ready():
 	position.y = 166
+#func _ready():
+	
 
 
 func _process(delta):
-	#print(position)
 	if ray_cast1.is_colliding():
 		print(ray_cast1.get_collider().position)
 		if ray_cast1.get_collider().name == "player":
 			
 			#velocity = (ray_cast1.get_collider().position - position).normalized() * 1000 * delta
+		if ray_cast1.get_collider().name == "player":			
+			rotation += -5 * delta
+			velocity = (ray_cast1.target_position).normalized().rotated(rotation) * 400 * delta
 			move_and_slide()
 	#if ray_cast2.is_colliding():
 		#print("raycast 2 collide")
@@ -28,3 +33,24 @@ func _process(delta):
 	#if ray_cast4.is_colliding():
 		#print("raycast 4 collide")
 	#testtesttest
+	if ray_cast2.is_colliding():
+		if ray_cast2.get_collider().name == "player":
+			rotation += -2 * delta
+			velocity = (ray_cast2.target_position).normalized().rotated(rotation) * 400 * delta
+			move_and_slide()
+	if ray_cast3.is_colliding():
+		if ray_cast3.get_collider().name == "player":
+			rotation += 2 * delta
+			velocity = (ray_cast3.target_position).normalized().rotated(rotation) * 400 * delta
+			move_and_slide()
+	if ray_cast4.is_colliding():
+		if ray_cast4.get_collider().name == "player":
+			rotation += 5 * delta
+			velocity = (ray_cast4.target_position).normalized().rotated(rotation) * 400 * delta
+			move_and_slide()
+	if ray_cast5.is_colliding():
+		if ray_cast5.get_collider().name == "player":
+			velocity = (ray_cast5.target_position).normalized().rotated(rotation) * 400 * delta
+			move_and_slide()
+		
+		
