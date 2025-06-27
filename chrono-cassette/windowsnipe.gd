27f241a9ce09a1f6cde8891ae10caf6a66db957e
Calynixion <160431995+Calynixion@users.noreply.github.com>
2025-06-27 12:20:24 -0400

    added window close

--- chrono-cassette/windowsnipe.gd
+++ chrono-cassette/windowsnipe.gd
@@ -4,7 +4,7 @@ extends Area2D
 @onready var invisible_color := Color (255,255,255,0)
 @onready var playernear := false
 @onready var killcontroller = get_tree().get_first_node_in_group("killcontrol")
-
+@onready var sprite = get_node("sprite")
 func _on_area_entered(area):
 	if area.is_in_group("player"):
 		playernear = true
@@ -21,3 +21,4 @@ func _process(delta):
 		label.modulate = lerp(label.modulate, invisible_color, delta*100.0) # makes text invisible
 	if Input.is_action_just_pressed("interact") and playernear:
 		killcontroller.get_node("sniper_timer").stop()
+		sprite.animation="close"
