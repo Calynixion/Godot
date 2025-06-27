dfd2497cb46c82145f5885b5cbd6126a0d1a7e58
Fwablo <fabriziomm2009@gmail.com>
2025-06-27 11:23:41 -0400

    sniper updated
    made snipers cause gameover and fixed a bug where 2 gameovers could happen at once

--- chrono-cassette/main.gd
+++ chrono-cassette/main.gd
@@ -2,13 +2,16 @@ extends Node2D
 
 @onready var game_over_scrn = $GameOverScrn
 @onready var player = get_tree().get_first_node_in_group("player")
-
+@onready var gameover := false #tells if the game is currently over
 
 
 func gameovernow():
-	game_over_scrn.gameover()
-	player.over()
+	if gameover == false: # this is to make sure you cant have multiple gameovers since it will try to 
+		gameover = true   #delete the player when it was already deleted
+		game_over_scrn.gameover()
+		player.over() #deletes player
 
 
 func _on_game_over_scrn_restarted():
+	gameover = false
 	get_tree().reload_current_scene()
