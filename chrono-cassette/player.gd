82830f911f20e6a3bf9a58286f75fd467dbbfdaa
Calynixion <160431995+Calynixion@users.noreply.github.com>
2025-06-28 14:14:11 -0400

    fabreeze how did you manage to delete the  window animation

--- chrono-cassette/player.gd
+++ chrono-cassette/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 @onready var cassette_path=get_tree().get_first_node_in_group("path")
 @onready var record_timer=get_node("record_timer")
+@onready var follow=cassette_path.get_node("path")
 const speed = 150.0
 var recording=false
 var cassette_length=2
@@ -21,9 +22,11 @@ func _physics_process(delta: float) -> void:
 		recording=true
 		held_path=false
 		cassette_path.curve.clear_points()
+		cassette_path.position=Vector2(0,0)
+		debug.text=str(cassette_path.position)+str(cassette_path.curve.point_count)
 	if record_timer.time_left<=0:
 		if recording==true:
-			debug.text=""
+			#debug.text=""
 			held_path=true
 			record_timer.start(cassette_length)
 			record_timer.paused=true
@@ -33,8 +36,8 @@ func _physics_process(delta: float) -> void:
 	elif held_path==true:
 		#function to move path to player position
 		cassette_path.position+=((direction*speed)/60)
-		debug.text="moving"
-	elif recording==true:
+		#debug.text="moving"
+	elif recording==true and direction!=Vector2(0,0):
 		record_cassette()
 	if Input.is_action_just_pressed("play_cassette") and recording==false:
 		play_cassette()
@@ -42,7 +45,7 @@ func _physics_process(delta: float) -> void:
 
 func record_cassette():
 	cassette_path.curve.add_point(position)
-	debug.text="active"
+	#debug.text="active"
 
 func play_cassette():
-	pass
+	follow.progress=50
