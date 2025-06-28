9f52bfe898ae5b40b60d81f45c9fbb5605da4187
Calynixion <160431995+Calynixion@users.noreply.github.com>
2025-06-28 10:27:28 -0400

    Working on cassette rewind

--- chrono-cassette/player.gd
+++ chrono-cassette/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
-
-
-const speed = 300.0
-
+@onready var cassette_path=$cassette_path
+@onready var record_timer=get_node("record_timer")
+const speed = 100.0
+var recording=false
 func over():
 	queue_free() #deletes the player when game is over
 
@@ -10,4 +10,19 @@ func _physics_process(delta: float) -> void:
 	var direction=Input.get_vector("left","right","up","down")
 	direction=direction.normalized()
 	velocity=direction * speed
+	if Input.is_action_just_pressed("record_cassette"):
+		record_timer.start(3)
+		record_timer.one_shot=true
+		recording=true
+		cassette_path.curve.clear_points()
+	if record_timer.is_stopped()==true:
+		recording=false
+	elif recording==true:
+		record_cassette()
 	move_and_slide()
+
+func record_cassette():
+	cassette_path.curve.add_point(Vector2(1,1))
+
+func play_cassette():
+	pass
