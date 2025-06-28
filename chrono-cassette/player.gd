861a15fa5d50b48de036339c0375a88c8ab3d7e5
Calynixion <160431995+Calynixion@users.noreply.github.com>
2025-06-28 11:37:11 -0400

    attempting to move path to player position

--- chrono-cassette/player.gd
+++ chrono-cassette/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 @onready var cassette_path=get_tree().get_first_node_in_group("path")
 @onready var record_timer=get_node("record_timer")
-const speed = 300.0
+const speed = 150.0
 var recording=false
-var cassette_length=90
+var cassette_length=2
+var held_path=false
 @onready var debug=get_node("debug")
 func over():
 	queue_free() #deletes the player when game is over
@@ -16,18 +17,29 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("record_cassette"):
 		record_timer.start(cassette_length)
 		record_timer.one_shot=true
+		record_timer.paused=false
 		recording=true
+		held_path=false
 		cassette_path.curve.clear_points()
-	if record_timer.is_stopped()==true:
+	if record_timer.time_left<=0:
+		if recording==true:
+			debug.text=""
+			held_path=true
+			record_timer.start(cassette_length)
+			record_timer.paused=true
 		recording=false
-		debug.text=""
+	elif held_path==true:
+		#function to move path to player position
+		debug.text="moving"
 	elif recording==true:
 		record_cassette()
+	if Input.is_action_just_pressed("play_cassette") and recording==false:
+		play_cassette()
 	move_and_slide()
 
 func record_cassette():
 	cassette_path.curve.add_point(position)
-	debug.text="active"+str(position)+str(cassette_path.global_position)
+	debug.text="active"
 
 func play_cassette():
 	pass
