95355c640ea9bd478e3c01975336b79bd7c4e8d9
Calynixion <160431995+Calynixion@users.noreply.github.com>
2025-07-01 13:06:37 -0400

    bullet optimizations

--- chrono-cassette/bullet.gd
+++ chrono-cassette/bullet.gd
@@ -4,6 +4,8 @@ var direction=Vector2(1,0)
 
 func _physics_process(delta: float) -> void:
 	position+=direction*speed
+	if global_position.x<0 or global_position.y<0 or global_position.x>12800 or global_position.y>7200:
+		queue_free()
 
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
